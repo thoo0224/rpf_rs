15d97f67699bfb6f20efc31a77b466f538e11e56
Thomas <40025225+thoo0224@users.noreply.github.com>
2022-10-17 00:15:55 +0200

    add native

---
+++ src/rpf_rs/src/native.rs
@@ -0,0 +1,11 @@
+use std::sync::Arc;
+
+use crate::archive::Archive;
+
+#[no_mangle]
+pub unsafe extern "C" fn create_archive<'a>(ptr: *const u8, len: usize) -> *const Archive<&'a [u8]> {
+    let data = std::slice::from_raw_parts(ptr, len);
+    let archive = Archive::from(data);
+
+    Arc::into_raw(Arc::new(archive)) as *const Archive<&'a [u8]>
+}
