217b689e0d19dfe308cfdec5ff0ec447ebd0f16b
Thomas <40025225+thoo0224@users.noreply.github.com>
2022-10-17 00:49:43 +0200

    native read header

--- src/rpf_rs/src/native.rs
+++ src/rpf_rs/src/native.rs
@@ -1,6 +1,30 @@
 use std::sync::Arc;
 
-use crate::archive::Archive;
+use crate::archive::{Archive, Header};
+
+#[repr(C, packed)]
+pub struct NativeHeader {
+    magic: u32,
+    entry_count: u32,
+    names_length: u32,
+    encryption_type: u32
+}
+
+impl From<Header> for NativeHeader {
+    fn from(header: Header) -> Self {
+        Self {
+            magic: header.magic,
+            entry_count: header.entry_count,
+            names_length: header.names_length,
+            encryption_type: header.encryption_type
+        }
+    }
+}
+
+#[repr(C)]
+pub enum NativeError {
+    None = 0
+}
 
 #[no_mangle]
 pub unsafe extern "C" fn create_archive<'a>(ptr: *const u8, len: usize) -> *const Archive<&'a [u8]> {
@@ -13,4 +37,15 @@ pub unsafe extern "C" fn create_archive<'a>(ptr: *const u8, len: usize) -> *cons
 #[no_mangle]
 pub unsafe extern "C" fn free_archive<'a>(archive: *const Archive<&'a [u8]>) {
     let _archive = Arc::from_raw(archive);
+}
+
+// todo: erorr handling
+#[no_mangle]
+pub unsafe extern "C" fn read_header<'a>(archive: *mut Archive<&'a [u8]>, header: &mut NativeHeader) -> NativeError {
+    let archive = &mut *archive;
+    let parsed_header = archive.read_header().unwrap();
+
+    *header = NativeHeader::from(parsed_header);
+
+    NativeError::None
 }
