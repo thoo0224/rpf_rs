8f5f4c5555bd3bb426e48a7bc2b2fcf599d3cdd8
Thomas <40025225+thoo0224@users.noreply.github.com>
2022-10-16 23:38:17 +0200

    add header test

--- src/rpf_rs/src/tests/archive_tests.rs
+++ src/rpf_rs/src/tests/archive_tests.rs
@@ -1,4 +1,5 @@
 use crate::{prelude::Archive, Result};
+use crate::archive::{Header, RPF_MAGIC};
 
 fn read_test_file<'a>() -> Result<&'a [u8]> {
     let data = include_bytes!("../../resources/dlc.rpf");
@@ -12,7 +13,16 @@ fn create_test_archive<'a>() -> Result<Archive<&'a [u8]>> {
 
 #[test]
 fn test_read_header() -> Result<()> {
-    let _archive = create_test_archive()?;
+    let mut archive = create_test_archive()?;
+    let header = archive.read_header()?;
+    header.validate()?;
+
+    assert_eq!(header, Header {
+        magic: RPF_MAGIC,
+        entry_count: 15,
+        names_length: 160,
+        decryption_type: 1313165391
+    });
 
     Ok(())
 }
